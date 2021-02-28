b4b8cd51b9301875c02aaeff32b16bdb7c2a8d70
Cyrill Vorfolomeev <firetimeloop@gmail.com>
2021-02-28 17:30:14 +0500

    After second code review_00

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,45 @@
-let profileName = document.querySelector(".profile__name");
-let profileBio = document.querySelector(".profile__bio");
+let profileName = document.querySelector('.profile__name');
+let profileBio = document.querySelector('.profile__bio');
 
-let formElement = document.querySelector(".popup__form");
+let formElement = document.querySelector('.popup__form');
 
-let nameInput = formElement.querySelector(".popup__input-text-box_type_name");
-let jobInput = formElement.querySelector(".popup__input-text-box_type_bio");
+let nameInput = formElement.querySelector('.popup__input-text-box_type_name');
+let jobInput = formElement.querySelector('.popup__input-text-box_type_bio');
 
-let likeButtons = document.querySelectorAll(".element__like-icon");
+let likeButtons = document.querySelectorAll('.element__like-icon');
 
-let popup = document.querySelector(".popup");
-let editButton = document.querySelector(".profile__edit-button");
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
 
-let closeButton = document.querySelector(".popup__close-button");
+let closeButton = document.querySelector('.popup__close-button');
 
 
 function openPopup() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileBio.textContent;
-  popup.classList.add("popup_opened");
+  popup.classList.add('popup_opened');
 }
 
 function closePopup() {
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 }
 
 function handleProfileSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileBio.textContent = jobInput.value;
-  closePopup()
+  closePopup();
 }
 
 
-editButton.addEventListener("click", openPopup);
+editButton.addEventListener('click', openPopup);
 
-closeButton.addEventListener("click", closePopup);
+closeButton.addEventListener('click', closePopup);
 
-formElement.addEventListener("submit", handleProfileSubmit);
+formElement.addEventListener('submit', handleProfileSubmit);
 
 likeButtons.forEach(function(button) {
-  button.addEventListener("click", function () {
-    button.classList.toggle("element__like-icon_pushed");
+  button.addEventListener('click', function () {
+    button.classList.toggle('element__like-icon_pushed');
   });
 });
